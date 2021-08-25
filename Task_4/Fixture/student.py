0a10ccf35a50cf22b6427bf214e76fe40cd4b81d
fedepacher <fede_pacher@hotmail.com>
2021-08-25 12:23:39 -0300

    add functions description

--- Task_4/Fixture/student.py
+++ Task_4/Fixture/student.py
@@ -5,14 +5,23 @@ class StudentDB:
     def __init__(self):
         self.__data = None
 
+    '''
+    Get student list from json file
+    param data_file     filemane with student info
+    '''
     def connect(self, data_file):
         with open(data_file) as json_file:
             self.__data = json.load(json_file)
 
+    '''
+    Get student in case existe in the list
+    param name      student name
+    '''
     def get_data(self, name):
         for student in self.__data['students']:
             if student['name'] == name:
                 return student
+
 
     def close(self):
         pass
--- Task_4/Fixture/test_student.py
+++ Task_4/Fixture/test_student.py
@@ -1,23 +1,29 @@
 from student import StudentDB
 import pytest
 
-@pytest.fixture(scope='module')#seteando el scope en module este metodo se llama solo una vez al comienzo y luego se ejecutan los test, de lo contrario se llamaria en cada test consumiendo memoria al pedo
+@pytest.fixture(scope='module')
 def db():
     print('----------------Setup method----------------')
     db = StudentDB()
     db.connect('students.json')
-    #return db si en lugar de retornar coloco 
-    yield db #va a devolver esta variable y cuadno se terminen los test continua despues del yield 
+    yield db 
     print('----------------Teardown method---------------')
     db.close()
 
-#si usamos el fixture a los metodos le tenems que ingerar el parametro db
+'''
+Test for student called Scott
+param db    student list
+'''
 def test_scott_data(db):    
     scott_data = db.get_data('Scott')
     assert scott_data['id'] == 1
     assert scott_data['name'] == 'Scott'
     assert scott_data['result'] == 'pass'
 
+'''
+Test for student called Mark
+param db    student list
+'''
 def test_mark_data(db):    
     mark_data = db.get_data('Mark')
     assert mark_data['id'] == 2
